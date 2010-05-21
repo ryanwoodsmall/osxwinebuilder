63ca2e09f47219a36073ec70c4baf52c71889998
rwoodsmall <rwoodsmall@8faad44e-e1f4-11de-9043-17b7bd24f792>
2010-05-21 21:41:45 +0000

    - Wine 1.2-rc1 -libpng 1.4.2

--- trunk/osxwinebuild.sh
+++ trunk/osxwinebuild.sh
@@ -21,7 +21,7 @@
 
 # wine version
 #   wine-X.Y.Z
-export WINEVERSION="1.1.44"
+export WINEVERSION="1.2-rc1"
 
 # timestamp
 export TIMESTAMP=$(date '+%Y%m%d%H%M%S')
@@ -602,12 +602,12 @@ function install_tiff {
 #
 # libpng
 #
-LIBPNGVER="1.4.1"
+LIBPNGVER="1.4.2"
 #LIBPNGVER="1.2.43"
 LIBPNGFILE="libpng-${LIBPNGVER}.tar.gz"
 LIBPNGURL="http://downloads.sourceforge.net/libpng/${LIBPNGFILE}"
 # 1.4.x SHA1 sum
-LIBPNGSHA1SUM="7a3488f5844068d67074f2507dd8a7ed9c69ff04"
+LIBPNGSHA1SUM="ab48f2452a020f0182af25c82b9aa719fe1a779d"
 # 1.2.x SHA1 sum
 #LIBPNGSHA1SUM="44c1231c74f13b4f3e5870e039abeb35c7860a3f"
 LIBPNGDIR="libpng-${LIBPNGVER}"
@@ -1428,7 +1428,7 @@ function install_wisotool {
 WINEVER=${WINEVERSION}
 WINEFILE="wine-${WINEVER}.tar.bz2"
 WINEURL="http://downloads.sourceforge.net/wine/${WINEFILE}"
-WINESHA1SUM="60f11693161b28ff9814949f2b6bbccee1d07a2c"
+WINESHA1SUM="31ea3a75ea560cd486fb58825242ac0a6fc664ca"
 WINEDIR="wine-${WINEVER}"
 function clean_wine {
 	clean_source_dir "${WINEDIR}" "${WINEBUILDPATH}"
